fafa731964de84e44e8117a0d6b085f57d6641ac
unknown <cristian>
2024-05-05 21:31:36 -0300

    modulo 2 completo

--- web/urls.py
+++ web/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('productosPorCategoria/<int:categoria_id>',
          views.productosPorCategoria, name="productosPorCategoria"),
     path('productosPorNombre', views.productosPorNombre, name='productosPorNombre'),
+    path('producto/<int:producto_id>',
+         views.productoDetalle, name="producto"),
 ]
